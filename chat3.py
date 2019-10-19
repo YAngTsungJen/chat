1f5cfd49c45c5ab51d8fbd29a9ef354e5cebee7a
Mike <q23362352@yahoo.com.tw>
2019-10-19 09:57:11 +0800

    add chat3.py

---
+++ chat3.py
@@ -0,0 +1,9 @@
+lines = []
+with open("3.txt", "r", encoding = "utf-8-sig") as f:
+	for line in f:
+		lines.append(line.strip())
+for line in lines:
+	s = line.split(" ")
+	time = s[0][:5]
+	name = s[0][5:]
+	print(time, name)
